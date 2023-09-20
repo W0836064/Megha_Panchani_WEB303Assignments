cf24151062892013b86f820f93a5cd370ec6c5e7
W0836064 <135310068+W0836064@users.noreply.github.com>
2023-09-19 22:35:24 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// WEB303 Assignment 2
+
+
+$(document).ready(function () {
+    // Initially hide the content div
+    $("#content").hide();
+
+    // Function to load content and animate
+    function loadAndAnimateContent(url) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: "html",
+            success: function (data) {
+                // Hide content with animation
+                $("#content").fadeOut(300, function () {
+                    // Clear the content
+                    $(this).empty();
+
+                    // Append the new content and fadeIn
+                    $(this).append(data).fadeIn(300);
+                });
+            }
+        });
+    }
+
+    // Click event for links
+    $("#content-wrapper a").on("click", function (e) {
+        e.preventDefault(); // Prevent default link behavior
+        var url = $(this).attr("href");
+        loadAndAnimateContent(url);
+    });
+});
