dea892f1a57e2f301666a0ede9b14f22559a27e3
W0836064 <135310068+W0836064@users.noreply.github.com>
2023-09-19 22:47:27 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,39 +1,33 @@
-$(document).ready(function() {
-    // Attach a click event listener to elements with the IDs 'prospect', 'convert', and 'retain'
-    $("#prospect, #convert, #retain").on("click", function(event) {
-        // Prevent the default behavior of the clicked link (e.g., navigating to a new page)
-        event.preventDefault();
-
-        // Get the ID of the clicked link ('prospect', 'convert', or 'retain')
-        var linkId = $(this).attr("id");
-
-        // Make an AJAX request to load content from a file with a name matching the link ID
+$(document).ready(function () {
+    // Event handler for when a link is clicked
+    $("a").click(function (event) {
+      event.preventDefault(); // Prevent the default behavior of the link
+  
+      var linkId = $(this).attr("id"); // Get the ID of the clicked link
+      var contentDiv = $("#content"); // Get the content div
+  
+      // Hide the content div with animation
+      contentDiv.hide(500, function () {
+        // Clear the contents of the content div
+        contentDiv.empty();
+  
+        // Use AJAX with the "GET" method to load content from the appropriate file
         $.ajax({
-            url: linkId + ".html", // Construct the URL based on the link ID
-            type: 'GET', // Specify the HTTP request method as 'GET'
-            dataType: "html", // Specify that we expect HTML content in the response
-            success: function(data) {
-                // Get a reference to the 'content' div
-                var $contentVal = $('#content');
-
-                // Hide the content div with a 400ms animation
-                $contentVal.hide();
-
-                // Set the HTML content of the 'content' div to the data received from the AJAX request
-                $contentVal.html(data);
-
-                // Fade in the 'content' div with a 1500ms animation
-                $contentVal.fadeIn(1500);
-
-                // Apply a border to the 'content' div
-                $contentVal.css({
-                    'border': '1.5px solid blue'
-                });
-            },
-            error: function() {
-                // Display an alert message if there is an error loading content
-                alert("Error loading content.");
-            }
+          url: linkId + ".html", // Assuming that the file names match the link IDs
+          method: "GET", // Specify the HTTP method as "GET"
+          dataType: "html",
+          success: function (data) {
+            // Append the loaded content to the content div
+            contentDiv.html(data);
+  
+            // Animate the content div to display the newly loaded HTML
+            contentDiv.show(500);
+          },
+          error: function () {
+            console.log("Error loading content.");
+          },
         });
+      });
     });
-});
+  });
+  
