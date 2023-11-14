dabbe5ae979388a111ed5beb145f3d003c6897da
W0836064 <135310068+W0836064@users.noreply.github.com>
2023-11-14 01:34:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,111 +1,117 @@
 $(document).ready(function () {
-
-
-    // Accordion functionality (keep your existing code)
-    $('.accordion-label').click(function () {
-        const panel = $(this).next('.accordion-panel');
-        $('.accordion-panel').not(panel).slideUp();
-        panel.slideToggle();
+    // Accordion functionality
+    $(".accordion-header").click(function () {
+      $(this).next(".accordion-content").slideToggle();
+      $(".accordion-content").not($(this).next()).slideUp();
     });
-
-    // Tab functionality (keep your existing code)
-    $('.tab-label').click(function () {
-        const index = $(this).index();
-        $('.tab-label').removeClass('active');
-        $(this).addClass('active');
-        $('.tab-pane').hide().eq(index).show();
+  
+    // Tabbed section functionality
+    $(".tab-headers li").click(function () {
+      var index = $(this).index();
+      $(".tab-content > div").hide().eq(index).show();
+      $(".tab-headers li").removeClass("active");
+      $(this).addClass("active");
     });
-
-    
-
-    $(document).ready(function () {
-        // Define a variable to track the sorting order for each column
-        const sortingOrder = {};
-    
-        // Function to sort the table rows based on a specific column
-        const sortTable = (column) => {
-            const table = $('.character-table tbody');
-            const rows = table.find('tr').get();
-            rows.sort(function (a, b) {
-                const keyA = $(a).find(`td:eq(${Object.keys(sortingOrder).indexOf(column)})`).text();
-                const keyB = $(b).find(`td:eq(${Object.keys(sortingOrder).indexOf(column)})`).text();
-    
-                if (sortingOrder[column] === 'asc') {
-                    return keyA.localeCompare(keyB);
-                } else {
-                    return keyB.localeCompare(keyA);
-                }
-            });
-            table.empty();
-            $.each(rows, function (index, row) {
-                table.append(row);
-            });
-        };
-    
-        // Function to toggle the sorting order and update the chevron
-        const toggleSorting = (column) => {
-            if (sortingOrder[column] === 'asc') {
-                sortingOrder[column] = 'desc';
-            } else if (sortingOrder[column] === 'desc') {
-                sortingOrder[column] = ''; // Reset sorting
-            } else {
-                sortingOrder[column] = 'asc';
-            }
-    
-            // Update the chevron
-            $('.character-table thead th a').each(function () {
-                const dataColumn = $(this).data('column');
-                const chevron = $(this).find('.chevron');
-                if (dataColumn === column) {
-                    if (sortingOrder[column] === 'asc') {
-                        chevron.html('&#x25B2;');
-                    } else if (sortingOrder[column] === 'desc') {
-                        chevron.html('&#x25BC;');
-                    } else {
-                        chevron.html(''); // No sorting, remove chevron
-                    }
-                } else {
-                    chevron.html('');
-                }
-            });
-        };
-    
-        // Handle clicking on table headings to sort data
-        $('.character-table thead th a').click(function (e) {
-            e.preventDefault();
-            const column = $(this).data('column');
-            toggleSorting(column);
-            sortTable(column);
-        });
-    
-        // Load character data from JSON and populate the table (keep your existing code)
-        // ...
-    });
-    
-    // Load character data from JSON and populate the table
-    $.ajax({
-        url: 'characters.json', // Replace with the correct path to your JSON file
-        dataType: 'json',
-        success: function (data) {
-            const table = $('.character-table tbody');
-            data.forEach(function (character) {
-                table.append(`
-                    <tr>
-                        <td>${character.firstName}</td>
-                        <td>${character.lastName}</td>
-                        <td>${character.age}</td>
-                        <td>${character.occupation}</td>
-                        <td>${character.city}</td>
-                        <td>${character.dateOfBirth}</td>
-                    </tr>
-                `);
-            });
-        },
-
-       
-        
-        error: function (error) {
-            console.error('Failed to load character data:', error);
+  });
+  
+  $(document).ready(function () {
+    let charactersData = [];
+    let originalData = []; // Store the original data for resetting
+    let sortedColumn = null;
+    let sortOrder = 1;
+  
+    // Function to render the table based on character data
+    function renderTable() {
+      const tbody = $("#characterTable tbody");
+      tbody.empty();
+  
+      charactersData.forEach(function (character) {
+        const row = $("<tr>");
+        row.append($("<td>").text(character.firstName));
+        row.append($("<td>").text(character.lastName));
+        row.append($("<td>").text(character.age));
+        row.append($("<td>").text(character.occupation));
+        row.append($("<td>").text(character.city));
+        row.append($("<td>").text(character.dateOfBirth));
+  
+        tbody.append(row);
+      });
+    }
+  
+    // Function to update table header with arrow symbols
+    function updateHeaderArrows() {
+      $("#characterTable th .sort-icon").html(""); // Clear existing icons
+      $("#characterTable th").each(function () {
+        if ($(this).data("sort") === sortedColumn) {
+          if (sortOrder === 1) {
+            $(this).find(".sort-icon").html("&#x25B2;"); // Up arrow
+          } else {
+            $(this).find(".sort-icon").html("&#x25BC;"); // Down arrow
+          }
         }
+      });
+    }
+  
+    // Function to sort the table by a given column
+    function sortTable(column) {
+      if (sortedColumn === column) {
+        sortOrder *= -1;
+      } else {
+        sortedColumn = column;
+        sortOrder = 1;
+      }
+  
+      charactersData.sort((a, b) => {
+        const aValue = a[column];
+        const bValue = b[column];
+        if (aValue < bValue) {
+          return -1 * sortOrder;
+        }
+        if (aValue > bValue) {
+          return 1 * sortOrder;
+        }
+        return 0;
+      });
+  
+      renderTable();
+      updateHeaderArrows();
+    }
+  
+    // Keep track of the number of times a column header is clicked
+    let clickCount = 0;
+  
+    // Handle click on table headings for sorting
+    $("#characterTable th").click(function () {
+      const column = $(this).data("sort");
+      sortTable(column);
+  
+      // Check if the table has been sorted for the third time
+      clickCount++;
+      if (clickCount === 3) {
+        // Reset the table to its original state
+        charactersData = [...originalData];
+        renderTable();
+        sortedColumn = null;
+        sortOrder = 1;
+        updateHeaderArrows();
+        clickCount = 0;
+      }
     });
-});
+  
+    // Handle resetting the table to the original data
+    $("#resetTable").click(function () {
+      charactersData = [...originalData];
+      renderTable();
+      sortedColumn = null;
+      sortOrder = 1;
+      updateHeaderArrows();
+    });
+  
+    // Load character data from the JSON file using AJAX
+    $.getJSON("characters.json", function (data) {
+      charactersData = data;
+      originalData = [...data];
+      renderTable();
+    });
+  });
+  
