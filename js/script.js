7d9f5219d9b8f77a550cb7f2a9aa44eafc6d35e2
W0836064 <135310068+W0836064@users.noreply.github.com>
2023-11-17 19:01:51 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,69 +1,108 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    let charactersData = [];
+  
     $.ajax({
-        url: 'characters.json',
-        dataType: 'json',
-        success: function(data) {
-            populateTable(data);
-
-            $('.filterBtn').on('click', function() {
-                const filter = $(this).data('filter');
-                filterCharacters(filter);
-            });
-
-            $('#searchInput').on('input', function() {
-                const searchTerm = $(this).val().toLowerCase();
-                searchCharacters(searchTerm);
-            });
-        }
+      url: "characters.json",
+      dataType: "json",
+      success: function (data) {
+        charactersData = data;
+        populateTable(charactersData);
+        updateFilterCounts(charactersData);
+  
+        $(".filterBtn").on("click", function () {
+          const filter = $(this).data("filter");
+          filterCharacters(filter);
+        });
+  
+        $("#searchInput").on("input", function () {
+          const searchTerm = $(this).val().toLowerCase();
+          searchCharacters(searchTerm);
+        });
+      }
     });
-
+  
     function populateTable(data) {
-        const tbody = $('#characterTable tbody');
-        tbody.empty();
-        data.forEach(character => {
-            const row = `<tr>
-                            <td>${character.firstName}</td>
-                            <td>${character.lastName}</td>
-                            <td>${character.info1}</td>
-                            <td>${character.info2}</td>
-                            <td>${character.info3}</td>
-                            <td>${character.info4}</td>
-                        </tr>`;
-            tbody.append(row);
-        });
-        updateFilterCounts(data);
+      const tbody = $("#characterTable tbody");
+      tbody.empty();
+      data.forEach((character) => {
+        const row = `<tr>
+            <td>${character.firstName}</td>
+            <td>${character.lastName}</td>
+            <td>${character.occupation}</td>
+            <td>${character.relationship}</td>
+            <td>${character.quirk}</td>
+            <td>${character.trait}</td>
+          </tr>`;
+        tbody.append(row);
+      });
     }
-
+  
     function filterCharacters(filter) {
-        const tbody = $('#characterTable tbody');
-        tbody.find('tr').hide();
-        tbody.find(`tr td:nth-child(2):icontains(${filter})`).parent().show();
+      const tbody = $("#characterTable tbody");
+      tbody.find("tr").hide();
+  
+      const filteredRows = charactersData.filter((character) => {
+        const lastNameFirstChar = character.lastName.charAt(0).toUpperCase();
+        return (
+          (filter === "A-M" &&
+            lastNameFirstChar >= "A" &&
+            lastNameFirstChar <= "M") ||
+          (filter === "N-Z" &&
+            lastNameFirstChar >= "N" &&
+            lastNameFirstChar <= "Z")
+        );
+      });
+  
+      const filteredIndices = filteredRows.map((filteredRow) =>
+        charactersData.findIndex((character) => character === filteredRow)
+      );
+  
+      filteredIndices.forEach((index) => {
+        tbody.find(`tr:eq(${index})`).show();
+      });
     }
-
+  
     function searchCharacters(searchTerm) {
-        const tbody = $('#characterTable tbody');
-        tbody.find('tr').each(function() {
-            const row = $(this);
-            const firstName = row.find('td:first').text().toLowerCase();
-            if (firstName.includes(searchTerm)) {
-                row.addClass('highlight');
-            } else {
-                row.removeClass('highlight');
-            }
-        });
-    }
-
-    $.extend($.expr[":"], {
-        "icontains": function(elem, i, match, array) {
-            return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+      const tbody = $("#characterTable tbody");
+      const rows = tbody.find("tr");
+  
+      rows.each(function () {
+        const row = $(this);
+        const firstName = row.find("td:first").text().toLowerCase();
+        const containsTerm = firstName.includes(searchTerm.toLowerCase());
+  
+        if (containsTerm) {
+          row.addClass("highlight").css({
+            "background-color": "darkgreen",
+            color: "white"
+          });
+        } else {
+          row.removeClass("highlight").css({
+            "background-color": "",
+            color: ""
+          });
         }
-    });
-
-    function updateFilterCounts(data) {
-        const rangeAMCount = data.filter(character => character.lastName.toUpperCase().charCodeAt(0) >= 65 && character.lastName.toUpperCase().charCodeAt(0) <= 77).length;
-        const rangeNZCount = data.length - rangeAMCount;
-
-        $('#rangeAM').text(rangeAMCount);
-        $('#rangeNZ').text(rangeNZCount);
+      });
+  
+      if (!searchTerm) {
+        // Clear search term, remove all highlights
+        rows.removeClass("highlight").css({
+          "background-color": "",
+          color: ""
+        });
+      }
     }
-});
+  
+    function updateFilterCounts(data) {
+      const rangeAMCount = data.filter(
+        (character) =>
+          character.lastName.toUpperCase().charCodeAt(0) >= 65 &&
+          character.lastName.toUpperCase().charCodeAt(0) <= 77
+      ).length;
+      const rangeNZCount = data.length - rangeAMCount;
+  
+      $("#rangeAM").text(`A - M (${rangeAMCount})`);
+      $("#rangeNZ").text(`N - Z (${rangeNZCount})`);
+    }
+  });
+  
